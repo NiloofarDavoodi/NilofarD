cda299447b87bb391f9ddce624563b96c6f9fcaa
NiloofarDavoodi <160004054+NiloofarDavoodi@users.noreply.github.com>
2024-02-14 11:43:16 +0330

    Add files via upload

---
+++ d(2).py
@@ -0,0 +1,11 @@
+name="niloofr davoodi"
+ascii_chars=[]
+
+for char in name:
+     ascii_chars.append(ord(char))
+
+
+print(ascii_chars)
+[110, 105, 108, 111, 111, 102, 114, 32, 100, 97, 118, 111, 111, 100, 105]
+
+
